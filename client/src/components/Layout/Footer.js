6ac2893726f4c5dfa151b2a5257f24bb917689c5
Menahasrinivas <menahasrinivas@gmail.com>
2024-09-16 16:03:20 +0530

    Add user

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light text-center p-4">
+        <h6 className="text-center"> All rights reserved &copy; Finance company Ltd</h6>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/deleteModel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Modal } from 'antd';
+
+const DeleteConfirmation = ({ visible, onConfirm, onCancel }) => {
+  return (
+    <Modal
+      title="Confirm Delete"
+      open={visible}
+      onOk={onConfirm}
+      onCancel={onCancel}
+      okText="Delete"
+      cancelText="Cancel"
+    >
+      <p>Are you sure you want to delete this transaction?</p>
+    </Modal>
+  );
+};
+
+export default DeleteConfirmation;
